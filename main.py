1a72e962741d2434c7d190f0d700c8e36bd49bda
incrediblemet <incrediblemet@gmail.com>
2026-04-24 10:48:29 +1000

    C: Accept user prompt as command-line argument using argparse

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -8,8 +9,12 @@ if not api_key:
     raise RuntimeError("API KEY NOT FOUND")
 client = genai.Client(api_key=api_key)
 
+parser = argparse.ArgumentParser(description="AI Agent")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
+
 response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    model="gemini-2.5-flash", contents=args.user_prompt)
 if response.usage_metadata is None:
     raise RuntimeError("likely a failed API request")
 else:
