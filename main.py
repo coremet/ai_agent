6df5a95adf5a0662f124517a9c688d7a4dd12acc
incrediblemet <incrediblemet@gmail.com>
2026-04-25 14:41:44 +1000

    E: Add optional command-line flag to toggle verbose output

--- main.py
+++ main.py
@@ -13,19 +13,22 @@ def main():
 
     parser = argparse.ArgumentParser(description="AI Agent")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
-    generate_content(client, messages)
+    generate_content(client, messages, args)
 
-def generate_content(client, messages):
+def generate_content(client, messages, args):
     response = client.models.generate_content(
         model="gemini-2.5-flash", contents=messages)
     if response.usage_metadata is None:
         raise RuntimeError("likely a failed API request")
     else:
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")    
+        if args.verbose: 
+            print(f"User prompt: {args.user_prompt}") 
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")    
     print(response.text)
 
  # If another file imports your file as a module, __name__ is set to the filename instead, so main() won't run automatically. 
